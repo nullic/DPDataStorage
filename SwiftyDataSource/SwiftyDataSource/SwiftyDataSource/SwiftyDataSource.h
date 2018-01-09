4e2f7f3a2db2173fbdb3227b9e962bfe99544778
Aleksey Bakhtin <aleksey.bahtin@effective-soft.com>
2018-01-09 13:35:30 +0300

    Add swifty datasource framework

---
+++ SwiftyDataSource/SwiftyDataSource/SwiftyDataSource/SwiftyDataSource.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyDataSource.h
+//  SwiftyDataSource
+//
+//  Created by Aleksey Bakhtin on 1/9/18.
+//  Copyright © 2018 EffectiveSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDataSource.
+FOUNDATION_EXPORT double SwiftyDataSourceVersionNumber;
+
+//! Project version string for SwiftyDataSource.
+FOUNDATION_EXPORT const unsigned char SwiftyDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDataSource/PublicHeader.h>
+
+
