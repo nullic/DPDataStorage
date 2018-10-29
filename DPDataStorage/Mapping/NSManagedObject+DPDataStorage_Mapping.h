31820aecde53eaf343b177cbecdd73e2e6617840
Dmitriy Petrusevich <nullic@gmail.com>
2018-10-29 09:48:39 +0300

    Add 'Lightweight Generics'

--- DPDataStorage/Mapping/NSManagedObject+DPDataStorage_Mapping.h
+++ DPDataStorage/Mapping/NSManagedObject+DPDataStorage_Mapping.h
@@ -11,11 +11,11 @@
 @interface NSManagedObject (DPDataStorage_Mapping)
 + (id _Nullable)transformImportValue:(id _Nonnull)value importKey:(NSString * _Nonnull)importKey propertyDescription:(NSPropertyDescription * _Nonnull)propertyDescription;
 
-+ (NSArray <__kindof NSManagedObject *>* _Nullable)updateWithArray:(NSArray * _Nonnull)array inContext:(NSManagedObjectContext * _Nonnull)context error:(NSError * _Nullable * _Nullable)out_error;
-+ (instancetype _Nullable)updateWithDictionary:(NSDictionary * _Nonnull)dictionary inContext:(NSManagedObjectContext * _Nonnull)context error:(NSError * _Nullable * _Nullable)out_error;
-- (BOOL)updateWithDictionary:(NSDictionary * _Nonnull)dictionary error:(NSError * _Nullable * _Nullable)out_error;
-- (BOOL)updateAttributesWithDictionary:(NSDictionary * _Nonnull)dictionary error:(NSError * _Nullable * _Nullable)out_error;
-- (BOOL)updateRelationshipsWithDictionary:(NSDictionary * _Nonnull)dictionary error:(NSError * _Nullable * _Nullable)out_error;
++ (NSArray <__kindof NSManagedObject *>* _Nullable)updateWithArray:(NSArray<NSDictionary<NSString *, id> *> * _Nonnull)array inContext:(NSManagedObjectContext * _Nonnull)context error:(NSError * _Nullable * _Nullable)out_error;
++ (instancetype _Nullable)updateWithDictionary:(NSDictionary<NSString *, id> * _Nonnull)dictionary inContext:(NSManagedObjectContext * _Nonnull)context error:(NSError * _Nullable * _Nullable)out_error;
+- (BOOL)updateWithDictionary:(NSDictionary<NSString *, id> * _Nonnull)dictionary error:(NSError * _Nullable * _Nullable)out_error;
+- (BOOL)updateAttributesWithDictionary:(NSDictionary<NSString *, id> * _Nonnull)dictionary error:(NSError * _Nullable * _Nullable)out_error;
+- (BOOL)updateRelationshipsWithDictionary:(NSDictionary<NSString *, id> * _Nonnull)dictionary error:(NSError * _Nullable * _Nullable)out_error;
 
 + (id _Nullable)transformExportValue:(id _Nullable)value exportKey:(NSString * _Nonnull)importKey propertyDescription:(NSPropertyDescription * _Nonnull)propertyDescription;
 
