fc6f2af6da6ba57b684379a635bbf22135d3bd71
Dmitriy Petrusevich <dmitriy.petrusevich@synesis-sport.by>
2019-01-08 15:29:24 +0300

    Add: DPSectionedArrayController class

---
+++ DPDataStorage/DPDataSource/DPSectionedArrayController.h
@@ -0,0 +1,36 @@
+//
+//  DPSectionedArrayController.h
+//  DPDataStorage
+//
+//  Created by Dmitriy Petrusevich on 1/8/19.
+//  Copyright © 2019 EffectiveSoft. All rights reserved.
+//
+
+#import <DPDataStorage/DPDataStorage.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPSectionedArrayController : DPArrayController
+@property (nonatomic, strong) NSSortDescriptor *sectionSortDescriptor;
+@property (nonatomic, copy, nullable) NSString *sectionKeyPath;
+ 
+- (instancetype)initWitDelegate:(id<DataSourceContainerControllerDelegate> _Nullable)delegate sectionKeyPath:(NSString * _Nullable)sectionKeyPath sectionSortDescriptor:(NSSortDescriptor *)sectionSortDescriptor NS_DESIGNATED_INITIALIZER;
+
+- (void)setObjects:(NSArray *)objects;
+
+- (void)insertObject:(id)object atIndextPath:(NSIndexPath *)indexPath NS_UNAVAILABLE;
+- (void)deleteObjectAtIndextPath:(NSIndexPath *)indexPath NS_UNAVAILABLE;
+- (void)reloadObjectAtIndextPath:(NSIndexPath *)indexPath NS_UNAVAILABLE;
+- (void)moveObjectAtIndextPath:(NSIndexPath *)indexPath toIndexPath:(NSIndexPath *)newIndexPath NS_UNAVAILABLE;
+
+- (void)insertSectionAtIndex:(NSUInteger)index NS_UNAVAILABLE;
+- (void)removeSectionAtIndex:(NSUInteger)index NS_UNAVAILABLE;
+- (void)reloadSectionAtIndex:(NSUInteger)index NS_UNAVAILABLE;
+- (void)removeEmptySections NS_UNAVAILABLE;
+
+- (void)addObjects:(NSArray *)objects atSection:(NSInteger)section NS_UNAVAILABLE;
+- (void)setObjects:(NSArray *)objects atSection:(NSInteger)section NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DPDataStorage/DPDataSource/DPSectionedArrayController.m
@@ -0,0 +1,59 @@
+//
+//  DPSectionedArrayController.m
+//  DPDataStorage
+//
+//  Created by Dmitriy Petrusevich on 1/8/19.
+//  Copyright © 2019 EffectiveSoft. All rights reserved.
+//
+
+#import "DPSectionedArrayController.h"
+
+@implementation DPSectionedArrayController
+
+- (instancetype)initWitDelegate:(id<DataSourceContainerControllerDelegate> _Nullable)delegate sectionKeyPath:(NSString *)sectionKeyPath sectionSortDescriptor:(NSSortDescriptor *)sectionSortDescriptor
+{
+    self.sectionKeyPath = sectionKeyPath;
+    self.sectionSortDescriptor = sectionSortDescriptor;
+    return [super initWithDelegate:delegate];
+}
+
+#pragma mark -
+
+- (void)setObjects:(NSArray *)objects {
+    [self startUpdating];
+
+    NSArray *sortedObjects = [objects sortedArrayUsingDescriptors:@[self.sectionSortDescriptor]];
+    NSMutableArray *sectionObjects = [NSMutableArray arrayWithObject:sortedObjects[0]];
+    NSInteger sectionIndex = 0;
+
+    NSComparator sectionComarator = ^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        return [@([objects indexOfObject:obj1]) compare:@([objects indexOfObject:obj2])];
+    };
+
+    for (NSInteger i = 1; i < sortedObjects.count; i++) {
+        if (self.sectionSortDescriptor.comparator(sortedObjects[i-1], sortedObjects[i]) != NSOrderedSame) {
+            [sectionObjects sortUsingComparator:sectionComarator];
+            [super setObjects:sectionObjects atSection:sectionIndex];
+            if (self.sectionKeyPath.length > 0) {
+                NSString *name = [[sectionObjects.firstObject valueForKeyPath:self.sectionKeyPath] description];
+                [self setSectionName:name atIndex:sectionIndex];
+            }
+
+            sectionObjects = [NSMutableArray array];
+            sectionIndex++;
+        }
+
+        [sectionObjects addObject:sortedObjects[i]];
+    }
+
+    [super setObjects:sectionObjects atSection:sectionIndex];
+
+    // Remove old sections
+    while ((sectionIndex + 1) < [self numberOfSections]) {
+        [super removeSectionAtIndex:sectionIndex + 1];
+    }
+
+    [self endUpdating];
+}
+
+@end
