bd8aa450c13cc28384fbe57e203a71db8497b212
Dmitriy Petrusevich <nullic@gmail.com>
2019-09-26 17:11:43 +0300

    Package: .dynamic type

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "DPDataStorage",
+            type: .dynamic,
             targets: ["DPDataStorage"]
         ),
     ],
